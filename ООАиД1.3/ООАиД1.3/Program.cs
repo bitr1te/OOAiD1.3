a092f67ef7d20616b6a3a8e3d120466d20f6d1be
f1z1k-dev <sanyavetkal@mai.ru>
2023-02-18 08:57:03 +0300

    Важная функция

--- ООАиД1.3/ООАиД1.3/Program.cs
+++ ООАиД1.3/ООАиД1.3/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace ООАиД1._3
+﻿using System;
+namespace ООАиД1._3
 {
     internal class Program
     {
@@ -13,11 +14,27 @@
             Console.WriteLine("Че молчишь");
             double Дима;
             Дима = 40; //кг
+            ОпределениеТипаДимы();
         }
 
-        static void Определениетипадимы()
+        static void ОпределениеТипаДимы()
         {
-
+            int i = 0;
+            string[] DimaArray = new string { "Нуб", "Про", "Перекач", "Нафидил", "В муте" };
+            bool otvet = false;
+            while (choslo > 4 || choslo < 0)
+            {
+                Console.WriteLine("Введите число: ");
+                int choslo = Convert.ToInt32(Console.ReadLine());
+            }
+            for (i = 0; i < DimaArray.Length; i++)
+            {
+                if (choslo == DimaArray[i])
+                {
+                    Console.WriteLine("Дима = " + DimaArray[i]);
+                    break;
+                }
+            }
         }
     }
 }
